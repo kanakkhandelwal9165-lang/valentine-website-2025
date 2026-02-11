0eeeb1ce6f8e9ec7ac29b3eb84551db36b8ce4fa
kanakkhandelwal9165-lang <kanakkhandelwal9165@gmail.com>
2026-02-11 22:39:10 +0000

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ window.addEventListener('DOMContentLoaded', () => {
     validateConfig();
 
     // Set texts from config
-    document.getElementById('valentineTitle').textContent = `${config.valentineName}, "Let's settle this!🤭"`;
+   document.getElementById('valentineTitle').textContent = "Let's settle this 🤭";
     
     // Set first question texts
     document.getElementById('question1Text').textContent = config.questions.first.text;
