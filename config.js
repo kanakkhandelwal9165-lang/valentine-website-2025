9cd37a2c48b8bf7b7a7559c0316c5ab64ed1801d
kanakkhandelwal9165-lang <kanakkhandelwal9165@gmail.com>
2026-02-11 00:23:56 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,17 +5,17 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jade",
+    valentineName: "Om",
 
     // The title that appears in the browser tab
-    // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Will You Be My Valentine? 💝",
+    // You can use emojis!💖🥹
+    pageTitle: "Will You Be My Valentine? ❤️",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
-        hearts: ['❤️', '💖', '💝', '💗', '💓'],  // Heart emojis
-        bears: ['🧸', '🐻']                       // Cute bear emojis
+        hearts: ['❤️', '😘', '💝', '🫂', '💓'],  // Heart emojis
+        bears: ['🧸', '🥹']                       // Cute bear emojis
     },
 
     // Questions and answers
@@ -42,16 +42,16 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "I knew it!Isse niche tum karte toh phir hota kalesh😛",  // Shows when they go past 5000%
+        high: "Bas itna???😢😔",              // Shows when they go past 1000%
+        normal: "! how dare you chose this?😒"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
-        emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
+        title: "You didn't had any option waise bhi!😛😘🫰🏻",
+        message: "Now ask me for valentine's date and get your gift and a big warm hug and a huge kiss!",
+        emojis: "❤️💖🥹❤️😘😛🫂"  // These will bounce around
     },
 
     // Color scheme for the website
